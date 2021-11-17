3eb1ad6523f0fcdd4d0d07eb5135977737fbb235
abikwa <abikalex8@gmail.com>
2021-11-17 11:46:29 +0200

    add clamsee couse model

--- django_demoapp/models.py
+++ django_demoapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    name_course = models.CharField(max_length=250)
+    numbrer_hour = models.IntegerField('Hour\' Number')
