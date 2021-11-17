c9743ba48afe74f9db212f72d4df998a67df4cfc
abikwa <abikalex8@gmail.com>
2021-11-17 11:47:14 +0200

    add courses def

--- django_demoapp/views.py
+++ django_demoapp/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'django_demoapp/home/index.html')
+
+def courses(request):
+    return render(request, 'django_demoapp/courses/index.html')
