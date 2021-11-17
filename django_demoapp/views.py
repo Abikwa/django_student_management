97b8a601f4520f2cfb5be125fc35d079746a6879
abikwa <abikalex8@gmail.com>
2021-11-17 15:40:00 +0200

    update responsible views

--- django_demoapp/views.py
+++ django_demoapp/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 
+from .models import Responsible
+
 # Create your views here.
 
 def home(request):
     return render(request, 'django_demoapp/home/index.html')
 
-def courses(request):
+def responsible(request):
+    responsibles = Responsible.objects.all()
+    context = {
+        'responsibles' : responsibles
+    }
+    return render(request, 'django_demoapp/responsibles/index.html', context)
+
+def course(request):
     return render(request, 'django_demoapp/courses/index.html')
