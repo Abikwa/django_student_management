18436e3f75b3d3fab746f9733dd4d3d0a0b05c5f
abikwa <abikalex8@gmail.com>
2021-11-17 15:37:40 +0200

    specifiant database

--- django_demo/settings.py
+++ django_demo/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_demo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_student',
+        'USER': 'root',
+        'PASSWORD': '1508',
+        'HOST': '127.0.0.1',
+        'PORT': '3307',
     }
 }
 
